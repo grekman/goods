d67b818b957a495a21e3aa2b617242fe88ea0414
grekman <grekman@gmail.com>
2017-05-04 14:43:16 +0300

    goods v 0.1.2 load images via js

--- js.js
+++ js.js
@@ -1,3 +1,13 @@
+
+var images = ['img/coffee01.png','img/coffee02.png','img/coffee03.png','img/coffee04.png','img/coffee05.png','img/coffee06.png'];
+var blocks = document.getElementsByClassName('block');
+
+document.getElementsByClassName('big-set')[0].innerHTML = '<img src="' + images[0] + '" alt="" class="big">'
+
+for (var i = 0; i < blocks.length; i++) {
+  blocks[i].innerHTML = '<img src="' + images[i] + '" alt="" class="small">';
+}
+
 var small = document.getElementsByClassName('small');
 for (var i = 0; i < small.length; i++) {
   small[i].onclick = function () {
